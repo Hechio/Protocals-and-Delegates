a592e26f6361e38d5357a1d6cc381e4de4e54e9c
Steve Hechio <stevehechio@gmail.com>
2020-08-09 13:07:57 +0300

    Initial Commit

---
+++ Protocals and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocals and Delegates
+//
+//  Created by Joel Personal on 8/9/20.
+//  Copyright © 2020 Steve Hechio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
