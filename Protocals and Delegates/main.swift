7b197f349adf893f800681287df58412b8d1fbeb
Steve Hechio <stevehechio@gmail.com>
2021-07-24 12:19:47 +0300

    understand protocal

--- Protocals and Delegates/main.swift
+++ Protocals and Delegates/main.swift
@@ -1,12 +1,53 @@
-//
-//  main.swift
-//  Protocals and Delegates
-//
-//  Created by Joel Personal on 8/9/20.
-//  Copyright © 2020 Steve Hechio. All rights reserved.
-//
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func asseccSituation() {
+        print("Can you tell me what happened")
+    }
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(hander: EmergencyCallHandler) {
+        hander.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does chest compression")
+    }
+    
+    
+}
 
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compression")
+    }
+    func useStethscope() {
+        print("Listening to heart beat")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sing staying alive by the BeeGess")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr...")
+    }
+}
+let hechio = EmergencyCallHandler()
+let queen = Surgeon(handler: hechio)
+hechio.asseccSituation()
+hechio.medicalEmergency()
